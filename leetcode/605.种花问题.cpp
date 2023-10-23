b8b58ba2381b26b723ecc9046e3b4ac7c7ef6d90
ekxs0109 <ekxs@outlook.com>
2023-10-23 10:17:03 +0800

    leetcode: array 605

---
+++ leetcode/605.种花问题.cpp
@@ -0,0 +1,48 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem605.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode.cn id=605 lang=cpp
+ *
+ * [605] 种花问题
+ */
+// [1,0,0,0,1] n=1
+//  p       q  q-p-1 4-0-1
+// [1,0,1,0,1,0,1] n=1
+//  p       q  3 5 7
+
+// @lc code=start
+class Solution
+{
+public:
+    bool canPlaceFlowers(vector<int> &flowerbed, int n)
+    {
+        int interval = 1, res = 0;
+
+        for (auto item : flowerbed)
+        {
+            if (item == 0)
+            {
+                interval++;
+            }
+            else
+            {
+                res += (interval - 1) / 2;
+                interval = 0;
+            }
+        }
+
+        if (interval > 1)
+        {
+            res += interval / 2;
+        }
+
+        return res>=n;
+    }
+};
+// @lc code=end
